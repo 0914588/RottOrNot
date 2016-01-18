dad4ffacdf4c88764807f2982bd9bec12e62edef
0919622 <0919622@hr.nl>
2016-01-18 13:25:03 +0100

    write to csv iens data

---
+++ py/test_iens.py
@@ -0,0 +1,38 @@
+__author__ = 'michel'
+
+from bs4 import BeautifulSoup
+
+import csv
+import urllib2
+import json
+url = urllib2.urlopen("https://www.iens.nl/restaurant/rotterdam/")
+
+content = url.read()
+
+soup = BeautifulSoup(content, "html.parser")
+
+with open('C:\Users\kevin\Documents\GitHub\RottOrNot\data\Iens.csv', 'w') as csvfile:
+    schrijf = csv.writer(csvfile, delimiter=' ',
+                            quotechar=' ', quoting=csv.QUOTE_MINIMAL)
+
+    for link in soup.select('article[class=listerItem]'):
+        # get the name
+        for string in link.a.strings:
+            print(string.strip())
+            name = string.strip()
+        # get the rating
+        for cijferdiv in link.find_all("div", class_="scoreMedium scoreMediumBest small-hide"):
+            for cijfer in cijferdiv.strings:
+                print(cijfer)
+        # get the address
+        addressText = ''
+        for addressdiv in link.find_all("address"):
+            for address in addressdiv.strings:
+                addressText += address.strip().replace(' ', '+')+'+'
+            print(addressText)
+            addressAPI = urllib2.urlopen("https://maps.googleapis.com/maps/api/geocode/json?address="+addressText);
+            addr = addressAPI.read()
+            j = json.loads(addr)
+            lat = j['results'][0]['geometry']['location']['lat']
+            lng = j['results'][0]['geometry']['location']['lng']
+            schrijf.writerow(name +'|'+ cijfer +'|' + addressText + '|' + str(lat) + '|' + str(lng))
